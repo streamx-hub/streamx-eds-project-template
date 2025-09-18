024c506fc74f57f62da18e20a4381a704d86e09c
Stanisław Ruchlicki <stanislaw.ruchlicki@ds.pl>
2025-09-18 11:52:50 +0200

    Remove search service

--- mesh/configs/opensearch/configuration/search-templates/format-search-templates.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-source ../utils.sh
-
-format_search_template V1.0.0.5.1__data_search_template
-format_search_template V1.0.0.6.3__products_search_template
-format_search_template V1.0.0.6.4__pages_search_template
